5750244426cbabe7ed3fdaa87ae7789e01570d5e
DarylBui <bui.yip.d@gmail.com>
2025-01-26 18:06:32 -0800

    case sensitive

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ d3.csv("data.csv").then(function (data) {
         var album = albums[i]['Album'];
         var cleaned_album = album.replace(/[\/\\#,+()~%.:*?<>{}]/g, '');
         d3.select("#alllist").insert("li").attr("class", "album-info").html(
-            "<div class=\"cover\">" + "<img src=\"" + "covers/" + cleaned_album + " " + albums[i]['Artist'] + "/image_1.jpg\"" +">"+"</a>"+"</div>" +
+            "<div class=\"cover\">" + "<img src=\"" + "covers/" + cleaned_album + " " + albums[i]['Artist'] + "/Image_1.jpg\"" +">"+"</a>"+"</div>" +
             "<div class=\"album\">" + (albums[i]['Album'])+"</a>"+"</div>" +
             "<div class=\"artist\">" + (albums[i]['Artist'])+"</a>"+"</div>" +
             "<div class=\"score\">" + (albums[i]['Score'])+"</a>"+"</div>"
         )
         if (albums[i]['Year'] == "2024") {
             d3.select("#alist").append("li").attr("class", "album-info").html(
-                "<div class=\"cover\">" + "<img src=\"" + "covers/" + cleaned_album + " " + albums[i]['Artist'] + "/image_1.jpg\"" +">"+"</a>"+"</div>" +
+                "<div class=\"cover\">" + "<img src=\"" + "covers/" + cleaned_album + " " + albums[i]['Artist'] + "/Image_1.jpg\"" +">"+"</a>"+"</div>" +
                 "<div class=\"album\">" + (albums[i]['Album'])+"</a>"+"</div>" +
                 "<div class=\"artist\">" + (albums[i]['Artist'])+"</a>"+"</div>" +
                 "<div class=\"score\">" + (albums[i]['Score'])+"</a>"+"</div>"
@@ -21,7 +21,7 @@ d3.csv("data.csv").then(function (data) {
         }
         if (albums[i]['Year'] == "2023") {
             d3.select("#blist").insert("li").attr("class", "album-info").html(
-                "<div class=\"cover\">" + "<img src=\"" + "covers/" + cleaned_album + " " + albums[i]['Artist'] + "/image_1.jpg\"" +">"+"</a>"+"</div>" +
+                "<div class=\"cover\">" + "<img src=\"" + "covers/" + cleaned_album + " " + albums[i]['Artist'] + "/Image_1.jpg\"" +">"+"</a>"+"</div>" +
                 "<div class=\"album\">" + (albums[i]['Album'])+"</a>"+"</div>" +
                 "<div class=\"artist\">" + (albums[i]['Artist'])+"</a>"+"</div>" +
                 "<div class=\"score\">" + (albums[i]['Score'])+"</a>"+"</div>"
